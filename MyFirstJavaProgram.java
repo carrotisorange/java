b87a17bfa128c5b4efc880900c0004e3bc911ed9
= <=>
2022-03-08 10:16:43 +0800

    initial commit.

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,10 @@
+class MyFirstJavaProgram{
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        int sum = x + y;
+
+        System.out.print(sum);
+    }
+}
