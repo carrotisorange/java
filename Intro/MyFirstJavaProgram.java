3aefe9f4a94302f3652f4806f59e02ecc1231bcb
= <=>
2022-03-09 11:54:11 +0800

    module 5 and module 6.

---
+++ Intro/MyFirstJavaProgram.java
@@ -0,0 +1,22 @@
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        int x = 1; 
+        int y = 1;
+
+        int sum = x + y;
+
+        // System.out.print('\n'+sum);
+        // double temp = 98.6;
+        // System.out.println("Body temperature is " + temp + " \u00b0F.");
+
+        if(1==1 || 2==3){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not equal");
+        }
+    }
+    
+
+}
+
+
---
+++ flowchart/MathDemo.java
@@ -0,0 +1,7 @@
+package flowchart;
+import java.lang.Math;
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.print(Math.pow(2,2));
+    }
+}
